668a5364989cb9c361654d1866264273c3738795
jiu <118248199+jiu6525@users.noreply.github.com>
2023-08-10 14:10:33 +0900

    Add files via upload

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.domain.item;
+
+import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.domain.exception.NotEnoughStockException;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+public abstract class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+
+    //==비즈시느 로직==//
+    // stock(재고) 증가
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    //stock(재고) 감소
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
+}
---
+++ jpashop/src/main/java/jpabook/jpashop/hello.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class hello {
+
+    private String data;
+}
